8c9a36bfd9d5b53d52d12416e32fe1016d07cbcc
KarlaSz <szymaszkiewiczk@gmail.com>
2024-10-19 19:01:08 +0200

    first

---
+++ typy-zmiennych.py
@@ -0,0 +1,65 @@
+print("siema1")
+print("'siema' TY ")
+print('siema "Karo"')
+
+a = 5
+b = 7.5
+c = 'piesek'
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(a + b)
+print(a * c) #za dziala, wyswietli 5 razy stringa
+#print(b + c)
+
+d = 'kotek'
+print(c + d)
+print(c + ' ' + d)
+wynik = (a + b) * 3
+print('Wynik rownania to',wynik,'PLN' ) 
+
+print(wynik)
+
+
+
+x = input('Ile masz zwierzat?')
+print('Więc mówisz że masz',x,'zwierzat')
+#print('Więc mówisz że masz',x,'zwierzat\') next line
+#print('Więc mówisz że masz',x,'zwierzat\t') tabulator
+
+
+----
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("siema1")
+print("'siema' TY ")
+print('siema "Karo"')
+
+a = 5
+b = 7.5
+c = 'piesek'
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(a + b)
+print(a * c) #za dziala, wyswietli 5 razy stringa
+#print(b + c)
+
+d = 'kotek'
+print(c + d)
+print(c + ' ' + d)
+wynik = (a + b) * 3
+print('Wynik rownania to',wynik,'PLN' ) 
+
+print(wynik)
+
+
+
+x = input('Ile masz zwierzat?')
+print('Więc mówisz że masz',x,'zwierzat')
+#print('Więc mówisz że masz',x,'zwierzat\') next line
+#print('Więc mówisz że masz',x,'zwierzat\t') tabulator
